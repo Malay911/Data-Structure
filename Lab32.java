ff858c69b131008e09bcabe963fee9a1f21a51b5
Malay911 <160609243+Malay911@users.noreply.github.com>
2024-07-06 18:04:39 +0530

    Update Lab32.java

--- Lab32.java
+++ Lab32.java
@@ -14,18 +14,32 @@ import java.util.*;
 public class Lab32 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of words: ");
-        int n = sc.nextInt();
-        String[] a = new String[n];
-        System.out.println("Enter " + n + " words: ");
-        for (int i=0;i<n;i++) {
-            a[i]=sc.next();
+        System.out.println("Enter string 1 :- ");
+        String a = sc.nextLine();
+        System.out.println("Enter string 2 :- ");
+        String b = sc.nextLine();
+        int temp = 0;
+        String n = a.toLowerCase();
+        String m = b.toLowerCase();
+        if(a.length()!=b.length()){
+            System.out.println("Given Strings are not Anagram Strings");
         }
-        Random r = new Random();
-        Double randomIndex = r.nextDouble(Math.random());
-        int index = randomIndex.intValue();
-        String word = a[index];
-        System.out.println("Enter an anagram of " + word + ": ");
-        String anagram = sc.next();
+        else{
+            for(int i=0;i<a.length();i++){
+                for(int j=0;j<b.length();j++){
+                    if(n.charAt(i)==m.charAt(j)){    
+                        temp++;
+                        break;
+                    }
+                }    
+            }
+        }
+        if(temp == a.length()){
+            System.out.println("Given Strings are Anagram Strings");
+        }
+        else{
+            System.out.println("Given Strings are not Anagram Strings");
+        }
+        sc.close();
     }
 }
