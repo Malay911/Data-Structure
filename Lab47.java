943e49c931902f0abf9523f96a5ed1217bb7f1ac
Malay911 <160609243+Malay911@users.noreply.github.com>
2024-07-22 11:17:28 +0530

    Update Lab47.java

--- Lab47.java
+++ Lab47.java
@@ -1,49 +1,50 @@
-import java.util.Scanner;
-import java.util.Stack;
+//Evaluation of Postfix using Stack
+import java .util.*;
 
 public class Lab47 {
-    public static int evaluatePostfix(String expression) {
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < expression.length(); i++) {
-            char ch = expression.charAt(i);
-            if (Character.isDigit(ch)) {
-                stack.push(ch - '0');
-            }
-            else {
-                int val1 = stack.pop();
-                int val2 = stack.pop();
-                
-                switch (ch) {
-                    case '+':
-                        stack.push(val2 + val1);
-                        break;
-                    case '-':
-                        stack.push(val2 - val1);
-                        break;
-                    case '*':
-                        stack.push(val2 * val1);
-                        break;
-                    case '/':
-                        stack.push(val2 / val1);
-                        break;
-                }
-            }
-        }
-        return stack.pop();
-    }
-    
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Enter a postfix expression: ");
-        String postfix = sc.nextLine();
-        
-        try {
-            int result = evaluatePostfix(postfix);
-            System.out.println("Evaluated Result: " + result);
-        } catch (Exception e) {
-            System.out.println("Invalid postfix expression");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter an expression:");
+        String s=sc.nextLine();
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char temp=s.charAt(i);
+            if(Character.isDigit(temp)){
+                st.push(Integer.parseInt(String.valueOf(temp)));
+            }
+            else{
+                int operand2=st.pop();
+                int operand1=st.pop();
+                int result=0;
+                switch(temp){
+                    case '+':
+                    result=operand1+operand2;
+                    break;
+
+                    case '-':
+                    result=operand1-operand2;
+                    break;
+
+                    case '*':
+                    result=operand1*operand2;
+                    break;
+
+                    case '/':
+                    result=operand1/operand2;
+                    break;
+
+                    case '^':
+                    result=(int) Math.pow(operand1,operand2);
+                    break;
+
+                    default:
+                    System.out.println("Invalid expression!");
+                    return;
+                }
+                st.push(result);
+            }
         }
-        sc.close();
+        int ans=st.pop();
+        System.out.println("Result: "+ans);
     }
 }
