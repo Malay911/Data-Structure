458768f70904caf23db8e2258e7a86ea51d880ec
Malay911 <160609243+Malay911@users.noreply.github.com>
2024-08-05 16:55:01 +1100

    Add files via upload

---
+++ Lab92.java
@@ -0,0 +1,27 @@
+//Bubble Sort
+import java.util.*;
+public class Lab92 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter arr length:");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter array elements:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Sorted array:");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
