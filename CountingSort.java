4f83f82b367f3d2cda2419b024d482b1a833037b
Malay911 <160609243+Malay911@users.noreply.github.com>
2024-09-02 16:56:24 +1100

    Add files via upload

---
+++ CountingSort.java
@@ -0,0 +1,47 @@
+
+//Counting Sort
+
+import java.util.*;
+
+public class CountingSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter arr length:");
+        int n = sc.nextInt();
+        System.out.println("Enter arr elements:");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        CountingSort(arr);
+        System.out.println("Sorted array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void CountingSort(int[] arr) {
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        int range = max - min + 1;
+        int[] count = new int[range];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i] - min]++;
+        }
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j++] = i + min;
+                count[i]--;
+            }
+        }
+    }
+}
