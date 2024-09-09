185d0c3d8a299753db9415ee5efeba81e793d5cd
Malay911 <160609243+Malay911@users.noreply.github.com>
2024-09-09 16:56:32 +0530

    Update Lab98.java

--- Lab98.java
+++ Lab98.java
@@ -25,11 +25,12 @@ public class Lab98 {
 
     public static void HeapSort(int[] arr, int n) {
         buildMaxHeap(arr, n);
-        int temp = arr[0];
-        arr[0] = arr[n - 1];
-        arr[n - 1] = temp;
-        n = n - 1;
-        HeapSort(arr, n);
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0);
+        }
     }
 
     public static void buildMaxHeap(int[] arr, int n) {
@@ -42,20 +43,17 @@ public class Lab98 {
         int largest = i;
         int left = 2 * i + 1;
         int right = 2 * i + 2;
-        if (left < n && arr[i] < arr[left]) {
+        if (left < n && arr[largest] < arr[left]) {
             largest = left;
-            arr[i] = arr[largest];
-            arr[largest] = arr[i];
         }
         if (right < n && arr[largest] < arr[right]) {
             largest = right;
-            arr[i] = arr[largest];
-            arr[largest] = arr[i];
-            heapify(arr, n, largest);
-        } else {
+        }
+        if (largest != i) {
             int temp = arr[i];
             arr[i] = arr[largest];
             arr[largest] = temp;
+            heapify(arr, n, largest);
         }
     }
 }
