0514cb3011b19f98fdde073d0a877f6fe89bf4f9
Malay911 <160609243+Malay911@users.noreply.github.com>
2024-09-30 18:59:25 +0530

    Update Lab72.java

--- Lab72.java
+++ Lab72.java
@@ -81,48 +81,46 @@ class DoublyLinkedList {
 
 public class Lab72 {
     public static void main(String[] args) {
-        DoublyLinkedList list = new DoublyLinkedList();
-        Scanner sc = new Scanner(System.in);
-        int choice, data, position;
-
-        do {
-            System.out.println("\nMenu:");
-            System.out.println("1. Insert at front");
-            System.out.println("2. Delete at position");
-            System.out.println("3. Insert at end");
-            System.out.println("4. Display all nodes");
+        Scanner sc=new Scanner(System.in);
+        DoublyLinkedList d1=new DoublyLinkedList();
+        while (true) {
+            System.out.println("1. Insert at first");
+            System.out.println("2. Insert at last");
+            System.out.println("3. Delete specific");
+            System.out.println("4. Display");
             System.out.println("5. Exit");
-            System.out.print("Enter your choice: ");
-            choice = sc.nextInt();
-
+            System.out.println("Enter your choice: ");
+            int choice = sc.nextInt();
             switch (choice) {
                 case 1:
-                    System.out.print("Enter data to insert at front: ");
-                    data = sc.nextInt();
-                    list.insertAtFront(data);
+                    System.out.println("Enter a number to be inserted at first:");
+                    int n =sc.nextInt();
+                    d1.insertAtFront(n);
+                    d1.display();
+                    System.out.println();
                     break;
                 case 2:
-                    System.out.print("Enter position to delete: ");
-                    position = sc.nextInt();
-                    list.deleteNode(position);
+                    System.out.println("Enter the position from where number is to be deleted:");
+                    int p =sc.nextInt();
+                    d1.deleteNode(p);
+                    d1.display();
+                    System.out.println();
                     break;
                 case 3:
-                    System.out.print("Enter data to insert at end: ");
-                    data = sc.nextInt();
-                    list.insertAtEnd(data);
+                    System.out.println("Enter a number to be inserted at last:");
+                    int m =sc.nextInt();
+                    d1.insertAtEnd(m);
+                    d1.display();
+                    System.out.println();
                     break;
                 case 4:
-                    System.out.print("Nodes in the list: ");
-                    list.display();
+                    d1.display();
                     break;
                 case 5:
-                    System.out.println("Exiting...");
-                    break;
+                    System.exit(0);
                 default:
-                    System.out.println("Invalid choice. Try again.");
+                    System.out.println("Invalid choice");
             }
-        } while (choice != 5);
-
-        sc.close();
+        }
     }
 }
